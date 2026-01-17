03cbd84cbd3cc9e783a393c22882308606f697e2
Abhijeet kothavale <shivani.kedar10@gmail.com>
2026-01-17 18:41:58 +0530

    Initial Commit

---
+++ ThirdSwiftUIProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThirdSwiftUIProject
+//
+//  Created by Abhijeet Kothavale on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThirdSwiftUIProject/ThirdSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdSwiftUIProjectApp.swift
+//  ThirdSwiftUIProject
+//
+//  Created by Abhijeet Kothavale on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ThirdSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
